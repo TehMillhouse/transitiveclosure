6ec991b45d90766c32eb8fdc40be3da70bbed1c7
Max Wagner <max@trollbu.de>
2014-07-17 20:42:55 +0200

    Fix warshall being told to output only edge counts

--- plot.py
+++ plot.py
@@ -18,7 +18,7 @@ def gen_dag(n, m):
 def init():
     global algorithms, sizes, graphs
     #algorithms = [("TLS", "count"), ("TLS", "array"), ("TLS", "matrix")]  # output format comparison:
-    algorithms = [(algo, "count") for algo in ["BFS", "DFS", "TLS", "TLS64", "WAR"]]
+    algorithms = [(algo, "count") for algo in ["BFS", "DFS", "TLS", "TLS64"]] + [("WAR","matrix")]
     sizes = [(int(2**i),2**17) for i in numpy.arange(10, 16, 0.5)]  # graph density comparison
     #sizes = [(2**i,2**(i+1)) for i in range(8, 20)]  # graph size comparison
     graphs = [(n, gen_dag(n, m)) for n, m in sizes]
